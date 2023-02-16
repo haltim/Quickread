1c2e32ae8bbdb53fbd2490a553bdb861bf6aa2b2
haltim <timoayomide2000@gmail.com>
2023-02-15 23:32:52 -0800

    ..in progress

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -4,21 +4,19 @@ import Vector from "../img/Vector.png";
 
 const BlogCard = (props) => {
   return (
-
-    <div className="blog-card">
-      <div className="blog-card-header">
-        <div className="blog-card-category">Entertainment</div>
-        <div className="blog-card-source">naijaloaded.com</div>
-      </div>
-      <div className="blog-card-content">
-        <div className="blog-card-title">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+    <div className='blog-card'>
+      <div className="blog-card-con">
+        <div className="blog-card-header">
+          <div className="blog-card-category">Entertainment</div>
+          <div className="blog-card-source">naijaloaded.com</div>
+        </div>
+        <div className="blog-card-content">
+          <div className="blog-card-title">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+        </div>
       </div>
       <div className="blog-card-image">
         <img src={Vector} alt={props.title} />
       </div>
-
-
-
 
     </div>
   );
